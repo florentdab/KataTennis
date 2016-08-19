be2f5c3348750b01a2921ec1599d2b688f1b8f81
2binfree <laurent.aubertin@free.fr>
2016-08-19 16:16:01 +0200

    kataTennis, basic solution with one game

---
+++ src/KataTennis.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace wcs;
+
+class KataTennis
+{
+    public static function score($score)
+    {
+        $scoreArray = str_split($score);
+        $progress = [0, 15, 30, 40];
+        $j1 = 0;
+        $j2 = 0;
+        
+        foreach ($scoreArray as $char){
+            switch($char){
+                case "1":
+                    $j1++;
+                    break;
+                case "2":
+                    $j2++;
+                    break;
+            }
+        }
+        return [$progress[$j1], $progress[$j2]];
+    }
+}
